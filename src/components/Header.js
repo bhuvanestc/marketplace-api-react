f33157303c8116b679147a056efdd00bdf112ead
bhuvan <bhuvanapraap@gmail.com>
2023-11-29 17:18:24 +0100

    navbar, header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function Header(){
+    return(
+        <>
+        <header className='container-fluid bg-danger text-white pt-5 pb-5'>
+            <div className='container'>
+                <h2>Market Place UI</h2>
+            </div>
+        </header>
+        </>
+    );
+}
+export default Header;
