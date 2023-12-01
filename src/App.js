dc1b28495c5947deab21a4db4fbc95647aef913f
bhuvan <bhuvanapraap@gmail.com>
2023-12-01 20:53:44 +0100

    advertisements, card

--- src/App.js
+++ src/App.js
@@ -10,37 +10,37 @@ const App = () => {
     {
       title: "Ad 1",
       description: "This is the first advertisement.",
-      image: "https://via.placeholder.com/150",
+      image: "https://via.placeholder.com/100",
       contact: "contact1@example.com",
     },
     {
       title: "Ad 2",
       description: "This is the second advertisement.",
-      image: "https://via.placeholder.com/150",
+      image: "https://via.placeholder.com/100",
       contact: "contact2@example.com",
     },
     {
       title: "Ad 3",
       description: "This is the third advertisement.",
-      image: "https://via.placeholder.com/150",
+      image: "https://via.placeholder.com/100",
       contact: "contact3@example.com",
     },
     {
       title: "Ad 4",
       description: "This is the fourth advertisement.",
-      image: "https://via.placeholder.com/150",
+      image: "https://via.placeholder.com/100",
       contact: "contact4@example.com",
     },
     {
       title: "Ad 5",
       description: "This is the fifth advertisement.",
-      image: "https://via.placeholder.com/150",
+      image: "https://via.placeholder.com/100",
       contact: "contact5@example.com",
     },
     {
       title: "Ad 6",
       description: "This is the sixth advertisement.",
-      image: "https://via.placeholder.com/150",
+      image: "https://via.placeholder.com/100",
       contact: "contact6@example.com",
     },
     // Add more advertisements as needed
